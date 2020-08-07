609adf81860d9ead5deea5a678813b2936e8edb3
Felipe Martins <felipemjesus@outlook.com>
2020-08-07 20:30:31 -0300

    Criado o componente da página Dar aulas

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
+
+import GiveClassesBgImg from '../../assets/images/give-classes-background.png';
+import { useNavigation } from '@react-navigation/native';
 
 function GiveClasses() {
-  return <View></View>
+  const { goBack } = useNavigation();
+
+  function handleNavigatorBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground 
+        source={GiveClassesBgImg} 
+        style={styles.content}
+        resizeMode="contain"
+      >
+        <Text style={styles.title}>
+          Quer ser um Proffy
+        </Text>
+        <Text style={styles.description}>
+          Pra começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton 
+        onPress={handleNavigatorBack}
+        style={styles.okButton}
+      >
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
 }
 
 export default GiveClasses;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -29,7 +29,9 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton 
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
